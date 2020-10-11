5b5c52a02b512d8efed2eaf4bd1d0e9a2eedbe82
Shadowchrist <sbhm.prasad@gmail.com>
2020-10-12 01:23:34 +0530

    UC2: Finding cheapest hotel : committed

--- src/com/workshop/hotelreservation/HotelReservation.java
+++ src/com/workshop/hotelreservation/HotelReservation.java
@@ -1,7 +1,10 @@
 package com.workshop.hotelreservation;
 
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
+import java.time.temporal.*;
 
 public class HotelReservation {
 	
@@ -16,7 +19,7 @@ public class HotelReservation {
 			String name=(new Scanner(System.in)).nextLine();
 			System.out.println("Enter weekday rates of the new hotel");
 			int weekdayRates=(new Scanner(System.in)).nextInt();
-			system.put(name, new HotelConstants(name,weekdayRates));
+			system.put(name.toUpperCase(), new HotelConstants(name,weekdayRates));
 			System.out.println("Want to add more hotels? (Y/N)");
 			char choice=(new Scanner(System.in)).next().charAt(0); 
 			if(choice=='N' || choice=='n')
@@ -28,14 +31,45 @@ public class HotelReservation {
 			else
 				continue;
 		}
-		System.out.println("Displaying all hotels in the system:\n");
-		for(String str: system.keySet())
-			system.get(str).displayHotelDetails();
+	}
+	
+	public static int getNumberOfDays(String date1, String date2)
+	{
+		int numberOfDays;
+		DateTimeFormatter myFormat=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate entryDate=LocalDate.parse(date1, myFormat);
+		LocalDate exitDate=LocalDate.parse(date2, myFormat);
+		numberOfDays=(int) ChronoUnit.DAYS.between(entryDate, exitDate);
+		return numberOfDays;
+	}
+	
+	@SuppressWarnings("resource")
+	public static void findCheapestHotel()
+	{
+		String cheapestHotel = "";
+		System.out.println("Enter Arrival Date: ");
+		String arrivalDate=(new Scanner(System.in)).nextLine();
+		System.out.println("Enter Departure Date: ");
+		String departureDate=(new Scanner(System.in)).nextLine();
+		int days=getNumberOfDays(arrivalDate,departureDate);
+		int minRate=Integer.MAX_VALUE;
+		for(String hotels: system.keySet())
+		{
+			if(system.get(hotels).weekdayRates*days<minRate)
+			{
+				minRate=system.get(hotels).weekdayRates*days;
+				cheapestHotel=hotels;
+			}
+		}
+		System.out.println("Cheapest hotel details: \n");
+		system.get(cheapestHotel).displayHotelDetails();
+		System.out.println("Total cost: "+ minRate);
 	}
 	
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Hotel Reservation System.");
 		enterNewHotelDetails();
+		findCheapestHotel();
 	}
 }
